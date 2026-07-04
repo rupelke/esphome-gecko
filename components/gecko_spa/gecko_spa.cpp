0d6a8f34d6233c43bcac5b39d06b56e02c99dd0f
rupelke <75843922+rupelke@users.noreply.github.com>
2026-07-04 09:54:57 +0200

    Update gecko_spa.cpp

--- components/gecko_spa/gecko_spa.cpp
+++ components/gecko_spa/gecko_spa.cpp
@@ -93,6 +93,15 @@ void GeckoSpa::send_circ_command(bool on) {
   send_i2c_message(cmd, 20);
   ESP_LOGI(TAG, "Sent circ %s command", on ? "ON" : "OFF");
 }
+void GeckoSpa::send_blower_command(bool on) {
+  uint8_t cmd[20] = {
+      0x17, 0x0A, 0x00, 0x00, 0x00, 0x17, 0x09, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x06, 0x46, config_version_, status_version_,
+      0x01, 0x6C, (uint8_t)(on ? 0x01 : 0x00), 0x00};
+  cmd[19] = calc_checksum(cmd, 20);
+  send_i2c_message(cmd, 20);
+  ESP_LOGI(TAG, "Sent blower %s command", on ? "ON" : "OFF");
+}
 
 void GeckoSpa::send_pump1_command(uint8_t state) {
   // P1 function ID: 0x03
